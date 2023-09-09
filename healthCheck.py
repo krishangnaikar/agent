467cd47e84c00f8869a4f1976944f086ecb65dc2
Purushotham <purushotham@PurushothamsAir.bbrouter>
2023-09-09 14:00:49 +0530

    [TM-8] Agent

---
+++ healthCheck.py
@@ -0,0 +1,74 @@
+import time
+import logging
+import logging.handlers
+import boto3
+from botocore.exceptions import BotoCoreError
+
+
+def setup_logger(log_file):
+    logger = logging.getLogger('my_logger')
+    logger.setLevel(logging.DEBUG)
+
+    # Create a rotating file handler to create a new log file every 10 minutes
+    handler = logging.handlers.TimedRotatingFileHandler(
+        log_file,
+        when="M",  # Rotate every 10 minutes
+        interval=10,  # Rotate every 10 minutes
+        backupCount=5  # Keep up to 5 backup log files
+    )
+
+    # Create a formatter
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(handler)
+    return logger
+def read_aws_credentials_from_json(file_path):
+    try:
+        import json
+        with open(file_path, 'r') as file:
+            credentials = json.load(file)
+            return credentials
+    except FileNotFoundError:
+        print(f"Credentials file '{file_path}' not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Invalid JSON format in '{file_path}'.")
+        return None
+
+def check_s3_bucket_health(credentials, bucket_name,logger):
+    try:
+        # Initialize the S3 client with the provided credentials
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=credentials['aws_access_key_id'],
+            aws_secret_access_key=credentials['aws_secret_access_key']
+        )
+
+        # Check if the bucket exists
+        response = s3_client.head_bucket(Bucket=bucket_name)
+        logger.info(f"Bucket {bucket_name} exists and is accessible.")
+
+        # Check if you can list objects in the bucket
+        response = s3_client.list_objects(Bucket=bucket_name)
+        if 'Contents' in response:
+            logger.info(f"Bucket {bucket_name} contains objects.")
+        else:
+            logger.info(f"Bucket {bucket_name} is empty.")
+
+        # You can perform additional health checks here, such as verifying permissions, encryption settings, etc.
+
+    except BotoCoreError as e:
+        logger.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    credentials_file_path = "credentials.json"
+    aws_credentials = read_aws_credentials_from_json(credentials_file_path)
+    log_file = "healthCheck.log"
+    logger = setup_logger(log_file)
+    if aws_credentials:
+        while True:
+            bucket_name = aws_credentials.get("bucket_name")
+            check_s3_bucket_health(aws_credentials, bucket_name,logger)
+            time.sleep(10)
