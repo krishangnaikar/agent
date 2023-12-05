dc3b84ce5ff5acdc1097ea5082f1bb1168dcb63a
keshavtrunil <keshav@flipkoins.com>
2023-12-05 19:16:01 +0530

    usage update

--- data_check.py
+++ data_check.py
@@ -367,24 +367,12 @@ def send_user_data(data, aws_credentials, logger):
                     for policy in policy_list:
                         if policy["PolicyName"]=="s3":
                             permissions = ["read","write"]
-                    payload_data = {
-                        "file_url": file_name,
-                        "user_name": username,
-                        "permissions": permissions
-                    }
-                    agent_files.append(payload_data)
-            payload_data = {
-                "file_url": data["file_url"],
-                "user_name": "Rahul",
-                "permissions": ["read","write","execute"]
-            }
-            agent_files.append(payload_data)
-            payload_data = {
-                "file_url": data["file_url"],
-                "user_name": "Rohit",
-                "permissions": ["read", "write", "execute"]
-            }
-            agent_files.append(payload_data)
+                            payload_data = {
+                                "file_url": file_name,
+                                "user_name": username,
+                                "permissions": permissions
+                            }
+                            agent_files.append(payload_data)
             payload = json.dumps(agent_files)
             headers = {
                 'Content-Type': 'application/json',
